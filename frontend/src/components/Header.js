bba5cf8d9608d9d378fefd77642f2305259b91e4
MeGallin <me@garyallin.uk>
2021-09-24 10:49:13 +0100

    screens working

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,14 +9,16 @@ const Header = () => {
           <Navbar.Brand href="/">ProShop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Nav.Link href="/cart">
-                <i className="fas fa-shopping-cart"></i>Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <i className="fas fa-user"></i>Sign In
-              </Nav.Link>
-            </Nav>
+            <div className="ms-auto">
+              <Nav>
+                <Nav.Link href="/cart">
+                  <i className="fas fa-shopping-cart"></i>Cart
+                </Nav.Link>
+                <Nav.Link href="/login">
+                  <i className="fas fa-user"></i>Sign In
+                </Nav.Link>
+              </Nav>
+            </div>
           </Navbar.Collapse>
         </Container>
       </Navbar>
