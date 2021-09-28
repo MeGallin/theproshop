e02f31a6a1f53441f20e14c6e94e98e25d256956
MeGallin <me@garyallin.uk>
2021-09-28 15:11:30 +0100

    error handler

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from './config/db.js';
 import cors from 'cors';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
@@ -15,6 +16,11 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+// @Error handling middleware
+app.use(notFound);
+app.use(errorHandler);
+// @Error handling middleware
 
 const PORT = process.env.PORT || 5000;
 const MODE = process.env.NODE_ENV;
