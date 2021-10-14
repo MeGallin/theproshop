881fa9be074574a73583792f0c0a99d95cabb682
MeGallin <me@garyallin.uk>
2021-10-14 11:50:35 +0100

    My orders summary

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -77,4 +77,12 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOderItems, getOrderById, updateOrderToPaid };
+// @description: Get logged in user orders
+// @route: GET /api/orders/myorders
+// @access: Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
+});
+
+export { addOderItems, getOrderById, updateOrderToPaid, getMyOrders };
