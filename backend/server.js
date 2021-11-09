bca40063384b935ec90d9dc43bbbd559a8db5cd2
MeGallin <me@garyallin.uk>
2021-11-09 14:47:54 +0000

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,6 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // This needed to accept json data
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -29,9 +25,19 @@ app.use('/api/upload', uploadRoutes);
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID),
 );
-
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')),
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
 
 // @Error handling middleware
 app.use(notFound);
