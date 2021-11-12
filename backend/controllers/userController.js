96395040d3ca11a91da5dfd26f05480d74fbe0d9
MeGallin <me@garyallin.uk>
2021-11-12 11:31:37 +0000

    registration email

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler';
 import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
+import nodemailer from 'nodemailer';
 
 // @description: Authenticate a user and get a token
 // @route: POST /api/users/login
@@ -66,6 +67,39 @@ const registerUser = asyncHandler(async (req, res) => {
       isAdmin: user.isAdmin,
       token: generateToken(user._id),
     });
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'mail.trilogywebsolutions.co.uk',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: 'info@trilogywebsolutions.co.uk',
+        pass: 'mayshop123456',
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Info May Shop" <info@trilogywebsolutions.co.uk>', // sender address
+      to: `${user.email}, me@garyallin.uk`, // list of receivers
+      subject: 'MayShop Registration', // Subject line
+      text: 'Hello world?', // plain text body
+      html: `
+    <h1>Hi ${user.name}</h1>
+    <p>You are successfully registered with MayShop</p>    
+    `, // html body
+    });
+
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   } else {
     res.status(400);
     throw new Error('Ivqlid user data');
