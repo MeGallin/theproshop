57fb292fe60177739873cb4e8bfbfc976b9d9007
MeGallin <me@garyallin.uk>
2021-12-03 13:53:11 +0000

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
-// import contactFormRoutes from './routes/contactFormRoutes.js';
 import confirmEmailRoutes from './routes/confirmEmailRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
@@ -27,7 +26,6 @@ app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
-// app.use('/api/send', contactFormRoutes);
 
 //PayPal direct route routes
 app.get('/api/config/paypal', (req, res) =>
@@ -36,6 +34,17 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')),
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
 
 // @Error handling middleware
 app.use(notFound);
